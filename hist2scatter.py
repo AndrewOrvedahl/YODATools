152d0be0418523c1374bcded9f162722f966198d
AndrewOrvedahl <AndrewOrvedahl@users.noreply.github.com>
2017-06-30 00:24:06 -0400

    Create hist2scatter.py
    Convert YODA files containing Histo1D objects to YODA files containing Scatter2D objects.

---
+++ hist2scatter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import yoda
+import sys
+
+def main(arg):
+    infile = arg
+    outfile = arg[:-5] + '_scatter.yoda'
+    aos = yoda.read(infile)
+    
+    scatter = []
+    hs = [h for h in aos.values()]
+    for h in hs:
+        s = h.mkScatter()
+        scatter.append(s)
+
+    yoda.write(scatter, outfile)
+    sys.exit(0)
+
+if __name__ == '__main__':
+    if(len(sys.argv) < 2):
+        print('Enter an input file!')
+        sys.exit(1)
+    else:
+	  #Enables shell wildcard use
+	  for arg in sys.argv[1:]
+        main(arg)
